94bf52fe80247fcb71d387c76d1cd9f5bfef0b54
Shane <70449042+carnealse@users.noreply.github.com>
2025-12-19 17:09:20 -0500

    Add files via upload
    upload lsb initial project

---
+++ lsbuilder/src/app/page.tsx
@@ -0,0 +1,9 @@
+import LogstashBuilder from "@/components/logstash-builder"
+
+export default function Page() {
+  return (
+    <div className="h-screen w-full">
+      <LogstashBuilder />
+    </div>
+  )
+}
