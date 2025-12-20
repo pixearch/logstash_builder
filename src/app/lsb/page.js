c8a1b20c1bc536deb0634ba1611d2acf87e46d62
Shane <70449042+carnealse@users.noreply.github.com>
2025-12-20 17:23:11 -0500

    Add Snowfall component to Home page

--- src/app/lsb/page.js
+++ src/app/lsb/page.js
@@ -1,9 +1,13 @@
 import LogstashBuilder from '@/components/LogstashBuilder';
+import Snowfall from '../components/Snowfall';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-slate-950">
-      <LogstashBuilder />
+    <main className="min-h-screen bg-slate-950 relative">
+      <Snowfall />
+      <div className="relative z-10">
+        <LogstashBuilder />
+      </div>
     </main>
   );
 }
